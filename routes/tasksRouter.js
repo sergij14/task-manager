6dd87025446c0ee55024f640c516740ede75912a
sergij14 <sergi.jajanidze@gmail.com>
2022-10-09 19:30:08 +0400

    setup additional routes and controllers

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAllTasks } = require("../constroller/tasksController");
+const {
+  getAllTasks,
+  deleteTask,
+  createTask,
+  updateTask,
+  getTask,
+} = require("../constroller/tasksController");
 
-router.route("/").get(getAllTasks);
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
